329355258d9ac85df20dc541725a20aa69fd05df
Lou1108 <72265570+Lou1108@users.noreply.github.com>
2024-11-11 09:49:43 +0100

    Added file for processing models (lab 1)

--- processingmodels.py
+++ processingmodels.py
@@ -1,12 +1,25 @@
 
 def start():
-    return
+    return 1
 
 def perceptualstep():
-    return
+    return 1
 
 def cognitivestep():
-    return
+    return 1
 
 def motorstep():
-    return
+    return 1
+
+
+def example1():
+    """
+
+    :return: total time of all cognitive processes
+    """
+    start_time = start()
+    perceptual_time = perceptualstep()
+    cog_time = cognitivestep()
+    motor_time = motorstep()
+
+    return start_time + perceptual_time + cog_time + motor_time
